6e43fa268968474a19a4cf75455d921dc590c0e0
hpaul-osi <hpaul@osisoft.com>
2017-09-19 18:18:52 -0700

    Added hooks for PITuningParameter and PIIdentity

---
+++ PISecurityAuditDSC/Configuration/PISecurityDSCBaselineHost.ps1
@@ -0,0 +1,29 @@
+﻿Configuration PISecurityAuditDSCBaseline
+{
+    param(
+        [String]$ComputerName = "localhost"
+         )
+
+    Import-DscResource -ModuleName PISecurityDSC
+
+    Node $ComputerName
+    {
+        PITuningParameter EditDays
+        {
+            Name = "EditDays"
+            Value = "180"
+            Ensure = "Present"
+            PIDataArchive = $ComputerName
+        }
+        
+        PIIdentity no_piadmin
+        {
+            Name = "piadmin"
+            AllowUseInTrusts = $false
+            AllowUseInMappings = $false
+            Ensure = "Present"
+            PIDataArchive = $ComputerName
+        }
+    }
+}
+PISecurityAuditDSCBaseline
