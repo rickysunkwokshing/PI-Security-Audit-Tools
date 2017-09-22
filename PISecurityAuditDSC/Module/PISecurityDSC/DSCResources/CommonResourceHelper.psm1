bf1e817c5afa500beb2d655cd9d1b44224ea4c93
hpaul-osi <hpaul@osisoft.com>
2017-09-22 08:33:03 -0700

    Improved reliability of Set function for PIDatabaseSecurity.

--- PISecurityAuditDSC/Module/PISecurityDSC/DSCResources/CommonResourceHelper.psm1
+++ PISecurityAuditDSC/Module/PISecurityDSC/DSCResources/CommonResourceHelper.psm1
@@ -15,10 +15,44 @@
         $Ensure = "Present"
         Foreach($Property in $($PIResource | Get-Member -MemberType Property | select -ExpandProperty Name))
         {
-            Write-Verbose "GetResult: $($Property): $($PIResource.$Property)."
+            Write-Verbose "GetResult: $($Property): $($PIResource.$Property.ToString())."
         }
     }
     return $Ensure
 }
 
-Export-ModuleMember -Function @( 'Get-PIResource_Ensure' )
+function Convert-PISecurityStringToHashTable
+{
+    param(
+        [parameter(Mandatory=$true)]
+        [System.String]
+        $SecurityString
+    )
+    [Hashtable]$SecurityTable = @{}
+    # Break pipe delimited ACL into entries
+    # Entry format: <Identity>: A(<Read, Write or 0>)
+    # Example: 'piadmins: A(r,w) | PIWorld: A(r) | PI Users: A(r)'
+    $SecurityEntries = $SecurityString.Split('|').Trim()
+
+    # Construct the desired security hashtable
+    foreach($Entry in $SecurityEntries)
+    {
+        # Split entry into the identity and access
+        $tokens = $Entry.Split(':').Trim()
+        # Assign accordingly
+        $Identity = $tokens[0]
+        $AccessRaw = $tokens[1]
+        # Convert the access to the same format as the current state representation.
+        switch ($AccessRaw)
+        {
+            'A(r,w)' {$Access = 'Read, Write';break}
+            'A(r)' {$Access = 'Read';break}
+            'A(w)' {$Access = 'Write';break}
+            'A()' {$Access = 0;break}
+        }
+        $SecurityTable.Add($Identity,$Access)
+    }
+    return $SecurityTable
+}
+
+Export-ModuleMember -Function @( 'Get-PIResource_Ensure', 'Convert-PISecurityStringToHashTable' )
--- PISecurityAuditDSC/Module/PISecurityDSC/DSCResources/xPIDatabaseSecurity/xPIDatabaseSecurity.psm1
+++ PISecurityAuditDSC/Module/PISecurityDSC/DSCResources/xPIDatabaseSecurity/xPIDatabaseSecurity.psm1
@@ -58,7 +58,36 @@ function Set-TargetResource
     }
     else
     { 
-        Set-PIDatabaseSecurity -Connection $Connection -Name $Name -Security $Security 
+        if($Name -eq 'PIBATCHLEGACY')
+        {
+            if($(Get-Service pibatch -ComputerName $PIDataArchive).Status -eq 'Running')
+            {
+                Set-PIDatabaseSecurity -Connection $Connection -Name $Name -Security $Security
+            }
+            else
+            {
+                Write-Verbose "PI Batch Subsystem must be running to edit database security for PIBATCHLEGACY"
+                Write-Verbose "PI Batch Subsystem is no longer needed.  It is recommended to disable the service"
+                Write-Verbose "and ignore the PIBATCHLEGACY database security entry."
+            }
+        }
+        elseif($Name -eq 'AFLINK')
+        {
+            if($(Get-Service piaflink -ComputerName $PIDataArchive).Status -eq 'Running')
+            {
+                Set-PIDatabaseSecurity -Connection $Connection -Name $Name -Security $Security
+            }
+            else
+            {
+                Write-Verbose "PI AF Link Subsystem must be running to edit database security for PIAFLINK"
+                Write-Verbose "If the system does not require MDB synchronization, you can disable the service"
+                Write-Verbose "and ignore teh PIAFLINK database security entry."
+            }
+        }
+        else
+        {
+            Set-PIDatabaseSecurity -Connection $Connection -Name $Name -Security $Security 
+        }
     }
 }
 
@@ -90,8 +119,10 @@ function Test-TargetResource
     if($PIResource.Ensure -eq 'Present')
     {
         if($Ensure -eq 'Present')
-        { 
-            if($PIResource.Security -eq $Security)
+        {
+            Write-Verbose "Testing against value: $Security"
+            $DoACLsMatch = Test-PIDatabaseSecurityACL -DesiredSecurity $Security -CurrentSecurity $PIResource.Security
+            if($DoACLsMatch)
             { 
                 $Result = $true 
             }
@@ -116,10 +147,49 @@ function Test-TargetResource
             $Result = $true 
         }
     }
-    Write-Verbose $Result
+    Write-Verbose "Test result: $Result"
     return $Result
 }
 
+function Test-PIDatabaseSecurityACL
+{
+    [CmdletBinding()]
+    [OutputType([System.Boolean])]
+    param(
+        [parameter(Mandatory=$true)]
+        [System.String]
+        $DesiredSecurity,
+        
+        [parameter(Mandatory=$true)]
+        [System.String]
+        $CurrentSecurity
+    )
+
+    $DesiredSecurityTable = Convert-PISecurityStringToHashTable -SecurityString $DesiredSecurity
+    $CurrentSecurityTable = Convert-PISecurityStringToHashTable -SecurityString $CurrentSecurity
+    if($CurrentSecurityTable.Count -ne $DesiredSecurityTable.Count)
+    {
+        return $false
+    }
+    else
+    {
+        foreach($Entry in $CurrentSecurityTable.GetEnumerator())
+        {
+            if(-not($DesiredSecurityTable.ContainsKey($Entry.Name)))
+            {
+                return $false
+            }
+            else
+            {
+                if($DesiredSecurityTable[$Entry.Name] -ne $Entry.Value)
+                {
+                    return $false
+                }
+            }
+        }
+        return $true
+    }
+}
 
 Export-ModuleMember -Function *-TargetResource
 
